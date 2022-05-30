aa6724bec6e7fcb14ca4c7b865eb957b7cdb00eb
Dmitry-757 <glv11.glv11.glv11@gmail.com>
2022-05-30 15:55:49 +0300

    some decorations

--- src/StoreServiceI.java
+++ src/StoreServiceI.java
@@ -3,5 +3,4 @@ public interface StoreServiceI {
     Invoice addInvoice(EInvoiceType type, Store store, Client client) throws Exception;
     void correctInvoice(Invoice invoice, EInvoiceType type, Store store, Client client);
     void removeInvoice(Invoice invoice);
-
 }
